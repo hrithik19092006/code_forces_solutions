0082563d594a0b1e4af8a6dd8c2523ab031d2b1b
hrithik19092006 <hrithik28289@gmail.com>
2026-07-19 14:40:01 +0530

    new

---
+++ 791A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a ;
+    int b ;
+    cin >> a >> b;
+    int count =0 ;
+    while(a <= b){
+        a *= 3;
+        b *= 2;
+        count++;
+    }
+    cout << count << '\n';
+    return 0;
+}
